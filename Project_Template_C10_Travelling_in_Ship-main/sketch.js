2005c9567d137e1f6ec03a71bb2243d15b7d7890
PandaGamer101 <87216356+PandaGamer101@users.noreply.github.com>
2021-07-30 13:56:24 -0700

    Add files via upload

---
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,17 @@
+
+function preload(){
+
+  seal = loadImage("sea.png");
+  shipImg1 = loadAnimation()
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
